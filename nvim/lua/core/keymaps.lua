432fd116f78fc850351f6fdce89f679e3054b823
stevar_roberto <133791398+stevarroberto@users.noreply.github.com>
2025-01-05 16:12:04 +0200

    Full folder

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true})
+local opts = { noremap = true, silent = true}
---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,10 @@
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = 'unnamedplus'
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.o.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+

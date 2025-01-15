60e0e2c5d4a42785f933bd0dd383e81053c50496
stevar_roberto <133791398+stevarroberto@users.noreply.github.com>
2025-01-15 16:36:29 +0100

    Update colortheme.lua

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,17 +1,11 @@
 return {
-	'shaunsingh/nord.nvim',
-	lazy = false,
-	priority = 1000,
-	config = function()
-			-- Example config in lua
-		vim.g.nord_contrast = true
-		vim.g.nord_borders = false
-		vim.g.nord_disable_background = false
-		vim.g.nord_italic = false
-		vim.g.nord_uniform_diff_background = true
-		vim.g.nord_bold = false
-
-		-- Load the colorscheme
-		require('nord').set()
-	end
+  'navarasu/onedark.nvim',
+  config = function()
+    require('onedark').setup {
+      style = 'cool',  -- You can choose between 'dark', 'darker', 'cool', 'warm', etc.
+      transparent = false,  -- Set to true if you prefer a transparent background
+    }
+    require('onedark').load()  -- Load the theme after configuration
+  end
 }
+
